71966ef3efb324990ee83ac2eb90a3a56c70ef49
xschen <xs0040@gmail.com>
2017-08-23 20:23:34 +0800

    sucessfully code highlight java

--- app/src/main/java/chen0040/github/com/androidgeeks4geeks/MainActivityFragment.java
+++ app/src/main/java/chen0040/github/com/androidgeeks4geeks/MainActivityFragment.java
@@ -33,7 +33,27 @@ public class MainActivityFragment extends Fragment {
             { WebView.setWebContentsDebuggingEnabled(true); }
         }
 
-        String html_string = "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/hello.css\" /></head><body><div id=\"hw\">Hello World</div><script type=\"text/javascript\" src=\"file:///android_asset/hello.js\"></script></body></html>";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><head>");
+        sb.append("<link rel=\"stylesheet\" href=\"file:///android_asset/default.min.css\">");
+        sb.append("<script src=\"file:///android_asset/highlight.min.js\"></script>");
+        sb.append("<script>hljs.initHighlightingOnLoad();</script>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<pre><code class=\"java\">");
+        sb.append("import java.utils.*\n");
+        sb.append("class Test&lt;T&gt; {\n");
+        sb.append("}\n");
+        sb.append("</code></pre>");
+        sb.append("</body></html>");
+
+
+
+        String html_string = sb.toString();
+
+
+        //String html_string = "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/hello.css\" /></head><body><div id=\"hw\">Hello World</div><script type=\"text/javascript\" src=\"file:///android_asset/hello.js\"></script></body></html>";
+
         browser.loadDataWithBaseURL("file:///android_asset/", html_string, "text/html", "UTF-8", "");
 
         return mainView;
