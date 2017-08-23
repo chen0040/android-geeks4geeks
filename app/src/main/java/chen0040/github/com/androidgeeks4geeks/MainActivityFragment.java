80e1cbadb9b8a5f39e15bdad3ef46951fe46bd8c
xschen <xs0040@gmail.com>
2017-08-23 09:48:43 +0800

    get web browser load local html and css from assets

--- app/src/main/java/chen0040/github/com/androidgeeks4geeks/MainActivityFragment.java
+++ app/src/main/java/chen0040/github/com/androidgeeks4geeks/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package chen0040.github.com.androidgeeks4geeks;
 
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View mainView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        WebView browser = (WebView)mainView.findViewById(R.id.webview);
+        browser.clearCache(true);
+        browser.clearHistory();
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (getActivity().getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))
+            { WebView.setWebContentsDebuggingEnabled(true); }
+        }
+
+        String html_string = "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/hello.css\" /></head><body><div id=\"hw\">Hello World</div><script type=\"text/javascript\" src=\"file:///android_asset/hello.js\"></script></body></html>";
+        browser.loadDataWithBaseURL("file:///android_asset/", html_string, "text/html", "UTF-8", "");
+
+        return mainView;
     }
 }
