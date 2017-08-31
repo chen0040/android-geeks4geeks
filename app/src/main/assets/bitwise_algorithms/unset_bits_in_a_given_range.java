49d619535b9c9a497464dce1cc2b7692473d83ac
xschen <xs0040@gmail.com>
2017-08-31 11:04:32 +0800

    unset bits in a given range

---
+++ app/src/main/assets/bitwise_algorithms/unset_bits_in_a_given_range.java
@@ -0,0 +1,23 @@
+/**
+ * Given a non-negative number n and two values l and r. The problem is to unset the bits in the range l to r in the binary representation of n, i.e, to unset bits from the rightmost lth bit to the rightmost rth bit.
+ *
+ * Constraint: 1 <= l <= r <= number of bits in the binary representation of n.
+ *
+ * Examples:
+ *
+ * Input : n = 42, l = 2, r = 5
+ * Output : 32
+ * (42)10 = (101010)2
+ * (32)10 = (100000)2
+ * The bits in the range 2 to 5 in the binary representation of 42 have been unset.
+ *
+ * Input : n = 63, l
+ * Output : 48
+ **/
+public class Solution {
+    public int unsetBitsInGivenRange(int n, int r, int l) {
+        int mask = ((1 << r) - 1) ^ ((1 << l) - 1);
+        mask = ~mask;
+        return n & mask;
+    }
+}
--- app/src/main/java/chen0040/github/com/androidgeeks4geeks/services/SolutionFactory.java
+++ app/src/main/java/chen0040/github/com/androidgeeks4geeks/services/SolutionFactory.java
@@ -68,6 +68,7 @@ public class SolutionFactory {
         solutions.add(new Solution("Basic", "Toggle the last m bits", "bitwise_algorithms/toggle_last_m_bits.java", field, topic));
         solutions.add(new Solution("Basic", "1 to n bit numbers with no consecutive 1s in binary representation.", "bitwise_algorithms/no_consecutive_ones.java", field, topic));
         solutions.add(new Solution("Basic", "Toggle bits in the given range", "bitwise_algorithms/toggle_bits_in_a_given_range.java", field, topic));
+        solutions.add(new Solution("Basic", "Unset bits in the given range", "bitwise_algorithms/unset_bits_in_a_given_range.java", field, topic));
         Log.i(TAG, "solutions: " + solutions.size());
 
         return solutions;
